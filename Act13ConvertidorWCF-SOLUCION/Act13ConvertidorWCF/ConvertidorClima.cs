5cf91d00fd96c38e037bb42a2adaa95b9f8e66b2
DracoGilga <cesar96707@gmail.com>
2023-04-26 23:09:57 -0600

    Creacion del convertidor de clima (interfaz, implementacion y POCO)

---
+++ Act13ConvertidorWCF-SOLUCION/Act13ConvertidorWCF/ConvertidorClima.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Act13ConvertidorWCF
+{
+    [DataContract]
+    public class ConvertidorClima
+    {
+        [DataMember]
+        public float centigrados { get; set; }
+        [DataMember]
+        public float fahrenheit { get; set; }
+    }
+}
