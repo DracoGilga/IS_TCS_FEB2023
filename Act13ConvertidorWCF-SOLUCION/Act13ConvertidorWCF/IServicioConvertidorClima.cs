af3d33456843e9f85d8189740e8fa3c7d222d2b9
DracoGilga <cesar96707@gmail.com>
2023-04-26 23:18:33 -0600

    creacion de los metodos abstractos en ISercicioConvertidorClima

--- Act13ConvertidorWCF-SOLUCION/Act13ConvertidorWCF/IServicioConvertidorClima.cs
+++ Act13ConvertidorWCF-SOLUCION/Act13ConvertidorWCF/IServicioConvertidorClima.cs
@@ -1,12 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.ServiceModel;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Act13ConvertidorWCF
 {
+    [ServiceContract]
     internal interface IServicioConvertidorClima
     {
+        [OperationContract]
+        ConvertidorClima convertidorClimaFahrenheit(float centigrados);
+        [OperationContract]
+        ConvertidorClima convertidorClimaCelntigrados(float fahrenheit);
     }
 }
