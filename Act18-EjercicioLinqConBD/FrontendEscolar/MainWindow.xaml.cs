24d5d60f83fbfb61b72e5afd290f011148fa5051
DracoGilga <cesar96707@gmail.com>
2023-05-29 00:33:50 -0600

    Creacion del GUI de login

---
+++ Act18-EjercicioLinqConBD/FrontendEscolar/MainWindow.xaml.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FrontendEscolar
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+
+        //Diseño de la pagina
+
+        private void ClickCerrar(object sender, MouseButtonEventArgs e)
+        {
+            this.Close();
+        }
+        private void ClickMinimizar(object sender, MouseButtonEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
+        }
+        private void MostrarToolTip(object sender, MouseEventArgs e)
+        {
+            ToolTip tt = (ToolTip)((Image)sender).ToolTip;
+            tt.IsOpen = true;
+        }
+        private void OcultarToolTip(object sender, MouseEventArgs e)
+        {
+            ToolTip tt = (ToolTip)((Image)sender).ToolTip;
+            tt.IsOpen = false;
+        }
+
+        //configuracion del textbox y del passwordbox
+        private void pwdBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            PasswordBox passwordBox = (PasswordBox)sender;
+            if (passwordBox.Password == "Contraseña")
+            {
+                passwordBox.Password = "";
+                passwordBox.Foreground = Brushes.Black;
+            }
+        }
+        private void pwdBox_LostFocus(object sender, RoutedEventArgs e)
+        {
+            PasswordBox passwordBox = (PasswordBox)sender;
+            if (string.IsNullOrWhiteSpace(passwordBox.Password))
+            {
+                passwordBox.Password = "Contraseña";
+                passwordBox.Foreground = Brushes.Gray;
+            }
+        }
+        private void TL_Usuario_GotFocus(object sender, RoutedEventArgs e)
+        {
+            TextBox textBox = (TextBox)sender;
+            if (textBox.Text == "Usuario")
+            {
+                textBox.Text = "";
+                textBox.Foreground = Brushes.Black;
+            }
+        }
+        private void TL_Usuario_LostFocus(object sender, RoutedEventArgs e)
+        {
+            TextBox textBox = (TextBox)sender;
+            if (string.IsNullOrWhiteSpace(textBox.Text))
+            {
+                textBox.Text = "Usuario";
+                textBox.Foreground = Brushes.Gray;
+            }
+        }
+
+    }
+}
---
+++ Act18-EjercicioLinqConBD/ServiciosLinqEscolar/Modelo/UsuarioDAO.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ServiciosLinqEscolar.Modelo
+{
+    public static class UsuarioDAO
+    {
+        public static List<usuario> obtenerUuarios()
+        {
+            DataClassesEscolarUVDataContext conexionBD = getConection();
+
+            IQueryable<usuario> usuariosBD = from usuarioQuery 
+                                             in conexionBD.usuario 
+                                             select usuarioQuery;
+
+            return usuariosBD.ToList();
+        }
+
+        public static usuario iniciarSesion(string username,string password)
+        {
+            DataClassesEscolarUVDataContext conexionBD = getConection();
+
+            var usuario = (from userLogin in conexionBD.usuario
+                           where userLogin.username == username &&
+                           userLogin.password == password
+                           select userLogin).FirstOrDefault();
+
+            return usuario;
+        }
+
+
+        public static Boolean GuardarUsuario(usuario usuarioNuevo) 
+        {
+            try
+            {
+                DataClassesEscolarUVDataContext conexionBD = getConection();
+
+                var usuario = new usuario()
+                {
+                    nombre = usuarioNuevo.nombre,
+                    apellidoPaterno = usuarioNuevo.apellidoPaterno,
+                    apellidoMaterno = usuarioNuevo.apellidoMaterno,
+                    username = usuarioNuevo.username,
+                    password = usuarioNuevo.password
+
+                    
+                };
+                conexionBD.usuario.InsertOnSubmit(usuario);
+                conexionBD.SubmitChanges();
+
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+
+        }
+        public static Boolean EditarUsuario(usuario UsuarioEdicion)
+        {
+            try
+            {
+                DataClassesEscolarUVDataContext conexionBD = getConection();
+                var usuario = (from usuarioEdicion in conexionBD.usuario
+                              where usuarioEdicion.idUsuario== UsuarioEdicion.idUsuario
+                              select usuarioEdicion).FirstOrDefault();
+                if(usuario != null )
+                {
+                    usuario.nombre = UsuarioEdicion.nombre;
+                    usuario.apellidoPaterno = UsuarioEdicion.apellidoPaterno;
+                    usuario.apellidoMaterno = UsuarioEdicion.apellidoMaterno;
+                    usuario.password = UsuarioEdicion.password;
+                    conexionBD.SubmitChanges() ;
+                    return true;
+                }
+                else { return false; }
+            }
+            catch(Exception ex)
+            {
+                return false;
+            }
+            
+        }
+        public static Boolean eliminarUsuario(int idUsuario)
+        {
+            try
+            {
+                DataClassesEscolarUVDataContext conexionBD = getConection();
+                usuario usuarioEliminar = (from usuario in conexionBD.usuario
+                                           where usuario.idUsuario == idUsuario
+                                           select usuario).FirstOrDefault();
+                if (usuarioEliminar != null)
+                {
+                    conexionBD.usuario.DeleteOnSubmit(usuarioEliminar);
+                    conexionBD.SubmitChanges();
+                    return true;
+                }else { return false; }
+
+            }catch(Exception ex) { return false; }
+            
+        }
+
+        public static DataClassesEscolarUVDataContext getConection()
+        {
+            return new DataClassesEscolarUVDataContext(global::System.Configuration.
+                ConfigurationManager.ConnectionStrings["escolarUVConnectionString"].
+                ConnectionString);
+
+        }
+    }
+}
